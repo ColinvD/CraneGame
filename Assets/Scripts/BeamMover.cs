3266ba72928809e18f4b6b141aa6aaa33be1f0d6
ColinvD <22950@ma-web.nl>
2020-09-01 13:43:23 +0200

    added crane beams

---
+++ Assets/Scripts/BeamMover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BeamMover : MonoBehaviour
+{
+    [SerializeField] private GameObject WidthBeam;
+    [SerializeField] private GameObject DepthBeam;
+
+    public void MoveBeams(Vector3 direction)
+    {
+        WidthBeam.transform.position += new Vector3(direction.x, direction.y, WidthBeam.transform.position.z);
+        DepthBeam.transform.position += new Vector3(DepthBeam.transform.position.x, direction.y, direction.z);
+    }
+}
