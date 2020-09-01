3f347f858ad67f0dfb508b429f16f81af540259d
ColinvD <22950@ma-web.nl>
2020-09-01 16:41:40 +0200

    added boundaries for the movement

--- Assets/Scripts/CraneMovement.cs
+++ Assets/Scripts/CraneMovement.cs
@@ -3,6 +3,8 @@
 public class CraneMovement : MonoBehaviour
 {
     [SerializeField] private float _craneSpeed = 0.1f;
+    [SerializeField] private Transform _minBoundary;
+    [SerializeField] private Transform _maxBoundary;
     [SerializeField] private GameObject _magnet;
     private BeamMover _beamMover;
     private InputManager _inputManager;
@@ -21,8 +23,31 @@ public class CraneMovement : MonoBehaviour
     private void MoveCrane()
     {
         Vector3 direction = GetDirection() * _craneSpeed;
+        direction = CheckForOutOfBounce(direction);
         _magnet.transform.position += direction;
         _beamMover.MoveBeams(direction);
+    }
+
+    private Vector3 CheckForOutOfBounce(Vector3 direction)
+    {
+        Vector3 newdir = new Vector3();
+
+        if (_magnet.transform.position.x + direction.x <= _maxBoundary.transform.position.x && _magnet.transform.position.x + direction.x >= _minBoundary.transform.position.x)
+        {
+            newdir.x = direction.x;
+        }
+        
+        if (_magnet.transform.position.y + direction.y <= _maxBoundary.transform.position.y && _magnet.transform.position.y + direction.y >= _minBoundary.transform.position.y)
+        {
+            newdir.y = direction.y;
+        }
+        
+        if (_magnet.transform.position.z + direction.z <= _maxBoundary.transform.position.z && _magnet.transform.position.z + direction.z >= _minBoundary.transform.position.z)
+        {
+            newdir.z = direction.z;
+        }
+
+        return newdir;
     }
     
     private Vector3 GetDirection()
